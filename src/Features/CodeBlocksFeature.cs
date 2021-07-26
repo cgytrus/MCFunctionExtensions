d424212c4063df6b01b8d3d0027be8ccb6f14d68
ConfiG <max1m1232@yandex.ru>
2021-07-26 23:05:50 +0300

    abstract code blocks feature

---
+++ src/Features/CodeBlocksFeature.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+
+namespace MCFunctionExtensions.Features {
+    public abstract class CodeBlocksFeature : IFeature {
+        public void Use(IReadOnlyList<string> readLines, List<string> newLines, Options options) {
+            List<string> inlineFunctionLines = new();
+            for(int i = 0; i < readLines.Count; i++) {
+                string line = readLines[i];
+
+                ProcessLine(ref i, line, readLines, newLines, options, inlineFunctionLines);
+            }
+        }
+        
+        private void ProcessLine(ref int index, string line, IReadOnlyList<string> readLines,
+            ICollection<string> newLines, Options options, ICollection<string> inlineLines) {
+            int startLine;
+            int inlineDepth;
+            try {
+                if(!IsInlineDeclaration(line, out string trimmedLine)) {
+                    newLines.Add(line);
+                    return;
+                }
+
+                newLines.Add(line.TrimEnd('{', ' '));
+                startLine = index;
+                inlineDepth = 0;
+                inlineLines.Clear();
+                index++;
+                while(index < readLines.Count) {
+                    line = readLines[index++].TrimStart();
+
+                    if(IsInlineDeclaration(line, out _)) inlineDepth++;
+                    if(IsInlineEnd(line)) {
+                        if(inlineDepth <= 0) {
+                            BlockEnd(options, inlineLines, trimmedLine);
+                            index--;
+                            break;
+                        }
+
+                        inlineDepth--;
+                    }
+
+                    inlineLines.Add(line);
+                }
+            }
+            catch(FunctionExtensionErrorException ex) {
+                throw new FunctionExtensionErrorException(index + 1, ex.Message, ex);
+            }
+
+            if(inlineDepth > 0) throw new FunctionExtensionErrorException(startLine + 1, "Braces not closed.");
+        }
+
+        protected abstract void BlockEnd(Options options, IEnumerable<string> inlineLines, string trimmedLine);
+
+        protected abstract bool IsInlineDeclaration(string line, out string trimmedLine);
+
+        protected virtual bool IsInlineEnd(string line) => line.Trim() == "}";
+    }
+}
--- src/Features/InlineFunctionsFeature.cs
+++ src/Features/InlineFunctionsFeature.cs
@@ -3,60 +3,21 @@ using System.Collections.Generic;
 using System.IO;
 
 namespace MCFunctionExtensions.Features {
-    public class InlineFunctionsFeature : IFeature {
-        public void Use(IReadOnlyList<string> readLines, List<string> newLines, Options options) {
-            List<string> inlineFunctionLines = new();
-            for(int i = 0; i < readLines.Count; i++) {
-                string line = readLines[i];
-
-                ProcessLine(ref i, line, readLines, newLines, options, inlineFunctionLines);
-            }
-        }
-        
-        private static void ProcessLine(ref int index, string line, IReadOnlyList<string> readLines,
-            ICollection<string> newLines, Options options, ICollection<string> inlineFunctionLines) {
-            int startLine;
-            int inlineDepth;
-            try {
-                string functionPath =
-                    ParseInlineFunctionDeclaration(options.functionsPath, options.useNamespace, line);
-                if(functionPath == null) {
-                    newLines.Add(line);
-                    return;
-                }
-
-                newLines.Add(line.TrimEnd('{', ' '));
-                startLine = index;
-                inlineDepth = 0;
-                inlineFunctionLines.Clear();
-                index++;
-                while(index < readLines.Count) {
-                    line = readLines[index++].TrimStart();
-
-                    if(IsInlineFunctionDeclaration(line, out string _, out string[] _)) inlineDepth++;
-                    if(line == "}") {
-                        if(inlineDepth <= 0) {
-                            File.WriteAllLines(functionPath, Program.CompileFunction(options, inlineFunctionLines));
-                            index--;
-                            break;
-                        }
-
-                        inlineDepth--;
-                    }
-
-                    inlineFunctionLines.Add(line);
-                }
-            }
-            catch(FunctionExtensionErrorException ex) {
-                throw new FunctionExtensionErrorException(index + 1, ex.Message, ex);
-            }
-
-            if(inlineDepth > 0) throw new FunctionExtensionErrorException(startLine + 1, "Braces not closed.");
+    public class InlineFunctionsFeature : CodeBlocksFeature {
+        protected override void BlockEnd(Options options, IEnumerable<string> inlineLines, string trimmedLine) {
+            string functionPath =
+                ParseInlineFunctionDeclaration(options.functionsPath, options.useNamespace,
+                    GetFunction(trimmedLine));
+            File.WriteAllLines(functionPath, Program.CompileFunction(options, inlineLines));
         }
 
-        private static bool IsInlineFunctionDeclaration(string line, out string trimmedLine, out string[] function) {
+        protected override bool IsInlineDeclaration(string line, out string trimmedLine) =>
+            IsInlineFunctionDeclaration(line, out trimmedLine);
+
+        private static string[] GetFunction(string trimmedLine) => trimmedLine.Split(' ')[^1].Split(':');
+
+        private static bool IsInlineFunctionDeclaration(string line, out string trimmedLine) {
             trimmedLine = line.TrimEnd();
-            function = null;
             
             if(!trimmedLine.EndsWith("{", StringComparison.InvariantCulture)) return false;
             
@@ -65,14 +26,14 @@ namespace MCFunctionExtensions.Features {
             
             if(args[^2] != "function") return false;
             
-            function = args[^1].Split(':');
-            return function.Length == 2;
+            return args[^1].Split(':').Length == 2;
         }
         
-        private static string ParseInlineFunctionDeclaration(string directory, string currNamespace, string line) {
-            if(!IsInlineFunctionDeclaration(line, out _, out string[] function)) return null;
-            if(function[0] != currNamespace)
-                throw new FunctionExtensionErrorException(0, "Tried declaring an inline function not in the current namespace.");
+        private static string ParseInlineFunctionDeclaration(string directory, string useNamespace,
+            IReadOnlyList<string> function) {
+            if(function[0] != useNamespace)
+                throw new FunctionExtensionErrorException(0,
+                    "Tried declaring an inline function not in the current namespace.");
             string functionName = function[1];
 
             return Path.Join(directory, $"{functionName}.mcfunction");
