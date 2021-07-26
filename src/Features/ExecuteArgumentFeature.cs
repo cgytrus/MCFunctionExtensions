b3f299677f5c3665ae7cc282586faddbee160807
ConfiG <max1m1232@yandex.ru>
2021-07-26 13:55:31 +0300

    abstract execute argument feature

---
+++ src/Features/ExecuteArgumentFeature.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MCFunctionExtensions.Features {
+    public abstract class ExecuteArgumentFeature : IFeature {
+        public void Use(IReadOnlyList<string> readLines, List<string> newLines, Options options) {
+            for(int i = 0; i < readLines.Count; i++) {
+                string line = readLines[i];
+                string[] args = line.Split(' ');
+
+                ProcessLine(newLines, line, args, i);
+            }
+        }
+        
+        private void ProcessLine(ICollection<string> newLines, string line, IReadOnlyList<string> args,
+            int lineIndex) {
+            if(!line.StartsWith("execute", StringComparison.InvariantCulture)) {
+                newLines.Add(line);
+                return;
+            }
+
+            bool containsArg = false;
+            for(int j = 0; j < args.Count; j++) {
+                if(args[j] == "run") break;
+                containsArg |= ProcessArg(newLines, args, j, lineIndex);
+            }
+
+            if(!containsArg) newLines.Add(line);
+        }
+
+        protected abstract bool ProcessArg(ICollection<string> newLines, IReadOnlyList<string> args, int i, int line);
+    }
+}
--- src/Features/ForLoopsFeature.cs
+++ src/Features/ForLoopsFeature.cs
@@ -1,35 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Text;
 
 namespace MCFunctionExtensions.Features {
-    public class ForLoopsFeature : IFeature {
-        public void Use(IReadOnlyList<string> readLines, List<string> newLines, Options options) {
-            for(int i = 0; i < readLines.Count; i++) {
-                string line = readLines[i];
-                string[] args = line.Split(' ');
-
-                ProcessLine(newLines, line, args, i);
-            }
-        }
-        
-        private static void ProcessLine(ICollection<string> newLines, string line, IReadOnlyList<string> args,
-            int lineIndex) {
-            if(!line.StartsWith("execute", StringComparison.InvariantCulture)) {
-                newLines.Add(line);
-                return;
-            }
-
-            bool containsFor = false;
-            for(int j = 0; j < args.Count; j++) {
-                if(args[j] == "run") break;
-                containsFor |= ProcessArg(newLines, args, j, lineIndex);
-            }
-
-            if(!containsFor) newLines.Add(line);
-        }
-
-        private static bool ProcessArg(ICollection<string> newLines, IReadOnlyList<string> args, int i, int line) {
+    public class ForLoopsFeature : ExecuteArgumentFeature {
+        protected override bool ProcessArg(ICollection<string> newLines, IReadOnlyList<string> args, int i, int line) {
             string arg = args[i];
             string nextArg = i < args.Count - 1 ? args[i + 1] : null;
             if(arg != "for") return false;
@@ -38,10 +12,8 @@ namespace MCFunctionExtensions.Features {
             if(!int.TryParse(nextArg, out int repeatTimes))
                 throw new FunctionExtensionErrorException(line + 1, "'for' must be an integer");
 
-            for(int k = 0; k < repeatTimes; k++) {
-                StringBuilder reconstructedLine = ReconstructLine(args);
-                newLines.Add(reconstructedLine.ToString());
-            }
+            string reconstructedLine = ReconstructLine(args).ToString();
+            for(int k = 0; k < repeatTimes; k++) newLines.Add(reconstructedLine);
 
             return true;
         }
